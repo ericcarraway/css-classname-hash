18cb44310d4eeff614e6d5c1864fae3bc9764bb8
Eric Carraway <eric@ericcarraway.com>
2022-02-06 17:53:04 +0100

    remove the restrictions RE: all upper case & all lower case (collisions)

--- index.ts
+++ index.ts
@@ -14,22 +14,6 @@ const isLetter = (char: string): boolean => /[a-zA-Z]/.test(char);
 
 const isLetterOrNumber = (char: string): boolean => /[A-Za-z0-9]/.test(char);
 
-const getIndexOfRightmostLetter = (str: string): number => {
-  for (let i: number = str.length - 1; i > 0; i -= 1) {
-    if (isLetter(str[i])) {
-      return i;
-    }
-  }
-
-  return 0;
-};
-
-const capitalizeCharAtIndex = (str: string, idx: number): string =>
-  `${str.substr(0, idx)}${str.charAt(idx).toUpperCase()}${str.substr(idx + 1)}`;
-
-const capitalizeRightmostLetter = (str: string): string =>
-  capitalizeCharAtIndex(str, getIndexOfRightmostLetter(str));
-
 const hashClassName = (originalClassName: string): string => {
   const rawHash: string = md5(originalClassName);
 
@@ -50,16 +34,6 @@ const hashClassName = (originalClassName: string): string => {
     // else, skip `char`
 
     idx += 1;
-  }
-
-  // if the hashed class name is all upper case,
-  // lowercase the first char/letter
-  if (cssClass === cssClass.toUpperCase()) {
-    cssClass = `${cssClass.charAt(0).toLowerCase()}${cssClass.slice(1)}`;
-  } else if (cssClass === cssClass.toLowerCase()) {
-    // if the hashed class name is all lower case,
-    // upper case the rightmost letter
-    cssClass = capitalizeRightmostLetter(cssClass);
   }
 
   return cssClass;
